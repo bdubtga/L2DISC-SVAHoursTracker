55340c61881245b3b84394141ed8dc4937d3f0b2
Benji Wallis <benji@benjiwallis.xyz>
2025-09-24 12:21:47 +1200

    login page html

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/')
 def portal():
     return render_template('portal.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
