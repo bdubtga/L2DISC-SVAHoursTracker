c487086a96e45120b8569997507fcb06909f0122
Benji <benji@benjiwallis.xyz>
2025-09-26 13:50:09 +1200

    update auth logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sqlite3
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'sdfcvy79t80wrfyrcvwn80h7wercv'
 
 # db connection
 con = sqlite3.connect('db.db', check_same_thread=False)
@@ -13,6 +14,9 @@ cur = con.cursor()
 # authentication
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    session.pop('user', None)
+    session.pop('valid', None)
+    
     # if form submitted
     if request.method == 'POST':
         username = request.form['username']
@@ -20,7 +24,9 @@ def login():
         
         user = cur.execute('SELECT * FROM user WHERE username = ? AND password = ?', (username, password)).fetchone()
         
-        if user:
+        if user: 
+            session['user'] = user[0]
+            session['valid'] = True
             return redirect('/')
         else:
             return render_template('login.html', error='wrong username or password')
@@ -32,18 +38,26 @@ def login():
 # main app routes
 @app.route('/')
 def portal():
+    if 'valid' not in session:
+        return redirect('/login')
     return render_template('portal.html')
 
 @app.route('/hours')
 def hours():
+    if 'valid' not in session:
+        return redirect('/login')
     return render_template('hours.html')
 
 @app.route('/recordofachievement')
 def recordofachievement():
+    if 'valid' not in session:
+        return redirect('/login')
     return render_template('recordofachievement.html')
 
 @app.route('/settings')
 def settings():
+    if 'valid' not in session:
+        return redirect('/login')
     return render_template('settings.html')
 
 if __name__ == '__main__':
