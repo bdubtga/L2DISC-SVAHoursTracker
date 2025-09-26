41cb3e6f6d02a30715558f22d4f72dbaa2542896
Benji <benji@benjiwallis.xyz>
2025-09-26 15:09:56 +1200

    settings

--- main.py
+++ main.py
@@ -54,11 +54,46 @@ def recordofachievement():
         return redirect('/login')
     return render_template('recordofachievement.html')
 
-@app.route('/settings')
+@app.route('/settings', methods=['GET', 'POST'])
 def settings():
     if 'valid' not in session:
         return redirect('/login')
-    return render_template('settings.html')
+    
+    user_id = session['user']
+    message = ""
+    
+    if request.method == 'POST':
+        if 'update_user' in request.form:
+            username = request.form['username']
+            cur.execute('UPDATE user SET username = ? WHERE id = ?', (username, user_id))
+            con.commit()
+            message = "Username updated!"
+        
+        elif 'update_school' in request.form:
+            school_id = request.form['school_id'] or None
+            cur.execute('UPDATE user SET school_id = ? WHERE id = ?', (school_id, user_id))
+            con.commit()
+            message = "School updated!"
+        
+        elif 'change_password' in request.form:
+            if request.form['new_password'] != request.form['confirm_password']:
+                return render_template('settings.html', error="Passwords do not match")
+            new_password = request.form['new_password']
+            cur.execute('UPDATE user SET password = ? WHERE id = ?', (new_password, user_id))
+            con.commit()
+            message = "Password changed!"
+    
+    user = cur.execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
+    schools = cur.execute('SELECT * FROM school ORDER BY school').fetchall()
+    current_school = None
+    if user[3]:
+        current_school = cur.execute('SELECT * FROM school WHERE id = ?', (user[3],)).fetchone()
+    
+    return render_template('settings.html', 
+                         user=user, 
+                         schools=schools, 
+                         current_school=current_school,
+                         message=message)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
